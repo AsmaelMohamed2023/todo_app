f58df6e363bb7a994c3426a841035a08d7a67dab
AsmaelMohamed2023 <asmaelshoky51@gmail.com>
2023-09-29 09:27:08 +0300

    customButton

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo/services/theme_services.dart';
 import 'package:todo/ui/pages/notification_screen.dart';
+import 'package:todo/ui/widgets/button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,7 +25,12 @@ class _HomePageState extends State<HomePage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: Container(),
+      body: Center(
+        child: MyButton(
+          label: 'Add Task',
+          onTap: () {},
+        ),
+      ),
     );
   }
 }
--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todo/ui/theme.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({Key? key}) : super(key: key);
+  const MyButton({Key? key, required this.label, required this.onTap})
+      : super(key: key);
+  final String label;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 100,
+        height: 45,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: primaryClr,
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          label,
+          style: const TextStyle(
+            color: white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
 }
